45b0ddcab2196de5bff57e29d554b44fb1c66364
VEDANT KOKANE <98166789+vedantkokane10@users.noreply.github.com>
2024-04-09 14:49:27 +0530

    Create 02_Topological_Sort_BFS.cpp

---
+++ 02_Graphs/02_Topological_Sort/02_Topological_Sort_BFS.cpp
@@ -0,0 +1,36 @@
+
+/*
+First Calculate the in degree for each node. Intially put all node in queue which have 0 in degree.
+Then pop front nodes and remove the edges from current node to the destination node. If the in degree
+of destination node is 0 then push that node in queue.
+Push front nodes in answer array.
+*/
+
+vector<int> topoSort(int V, vector<int> adj[]) 
+{
+    vector<int> ans;
+    vector<int> inDegree(V,0);
+    for(int i=0;i<V;i++){
+        for(auto x:adj[i]){
+            inDegree[x]++;
+        }
+    }
+    queue<int> q1;
+    for(int i=0;i<V;i++){
+        if(inDegree[i] == 0){
+            q1.push(i);
+        }
+    }
+    while(!q1.empty()){
+        int node = q1.front();
+        ans.push_back(node);
+        q1.pop();
+        for(auto x:adj[node]){
+            inDegree[x]--;
+            if(inDegree[x] == 0){
+                q1.push(x);
+            }
+        }
+    }
+    return ans;
+}
