e2c462231a01ec67b52a069206cee75118f95b11
VEDANT KOKANE <98166789+vedantkokane10@users.noreply.github.com>
2024-04-13 18:09:32 +0530

    Update 07_Permutations.cpp

--- 01_Recursion/07_Permutations.cpp
+++ 01_Recursion/07_Permutations.cpp
@@ -4,13 +4,19 @@ Example 1:
 Input: nums = [1,2,3]
 Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 
+                     [1, 2, 3]
+                /         |          \
+       [1, 2, 3]    [1, 3, 2]    [2, 1, 3]
+        /   \          /   \         /   \
+[1, 2, 3] [1, 3, 2] [1, 2, 3] [1, 3, 2] [2, 1, 3] [2, 3, 1]
+
+
+
 Example 2:
 Input: nums = [0,1]
 Output: [[0,1],[1,0]]
 
-Example 3:
-Input: nums = [1]
-Output: [[1]]
+
 */
 
 void helper(int pos,vector<int> &nums,vector<vector<int>> &ans){
