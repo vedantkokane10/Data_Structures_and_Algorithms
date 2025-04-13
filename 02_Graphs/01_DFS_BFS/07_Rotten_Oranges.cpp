99738af8182b290f4222f7a029e4b47fc38a4eac
VEDANT KOKANE <98166789+vedantkokane10@users.noreply.github.com>
2025-04-13 13:12:35 +0530

    Update 07_Rotten_Oranges.cpp

--- 02_Graphs/01_DFS_BFS/07_Rotten_Oranges.cpp
+++ 02_Graphs/01_DFS_BFS/07_Rotten_Oranges.cpp
@@ -15,47 +15,48 @@ Explanation: The orange in the bottom left corner (row 2, column 0) is never rot
 */
 
 int orangesRotting(vector<vector<int>>& grid) {
-  int n = grid.size();
-  int m = grid[0].size();
-  vector<vector<int>> vis(n,vector<int>(m,0));
-  queue<pair<pair<int,int>,int>> q1;
-  int totalOnes = 0;
-  for(int i=0;i<n;i++){
-      for(int j=0;j<m;j++){
-          if(grid[i][j] == 2){
-              vis[i][j] = 2;
-              q1.push({{i,j},0});
-          }
-          if(grid[i][j] == 1){
-              totalOnes++;
-          }
-      }
-  }
-  int dr[4] = {0,-1,0,1};
-  int dc[4] = {-1,0,1,0};
-  int time = 0;
-  int ones = 0;
-  while(!q1.empty()){
-      int row = q1.front().first.first;
-      int col = q1.front().first.second;
-      int t = q1.front().second;
-      time = max(t,time);
-      q1.pop();
-      for(int i=0;i<4;i++){
-          int nrow = row + dr[i];
-          int ncol = col + dc[i];
-          if((nrow >= 0 && nrow < n) && (ncol >= 0 && ncol < m)){
-              if(vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
-                  vis[nrow][ncol] = 2;
-                  q1.push({{nrow,ncol},t+1});
-                  ones++;
-              }
-          }
-      }
-  }
+    int m = grid.size();
+    int n = grid[0].size();
+    queue<pair<pair<int,int>,int>> q1;
+    vector<vector<bool>> vis(m,vector<bool>(n,0));
+    int noOfOnes = 0;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j] == 2){
+                q1.push({{i,j},0});
+                vis[i][j] = 1;
+            }
+            if(grid[i][j] == 1){
+                noOfOnes++;
+            }
+        }
+    }
+    int dr[4] = {0,-1,0,1};
+    int dc[4] = {-1,0,1,0};
+    int time = 0;
+    int ones = 0;
+    while(!q1.empty()){
+        int row = q1.front().first.first;
+        int col = q1.front().first.second;
+        int currentTime = q1.front().second;
+        q1.pop();
+        time = max(time,currentTime);
+        for(int i=0;i<4;i++){
+            int nrow = row + dr[i];
+            int ncol = col + dc[i];
+            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n){
+                if(!vis[nrow][ncol] && grid[nrow][ncol] == 1){
+                    ones++;
+                    q1.push({{nrow,ncol},currentTime+1});
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+    }
 
-  if(totalOnes != ones){
-      return -1;
-  }
-  return time;
+    if(noOfOnes != ones){
+        return -1;
+    }
+    return time;
+
 }
